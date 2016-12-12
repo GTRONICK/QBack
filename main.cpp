246ee6736e2028f78790ff2a066efa0f1d6c5e12
Jaime Quiroga <gtronick@gmail.com>
2016-12-12 08:26:27 -0500

    Splash routine added
    Se agrega código para mostrar el splash al inicio.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include "bumain.h"
 #include <QApplication>
+#include <QSplashScreen>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QSplashScreen *splash = new QSplashScreen;
+    splash->setPixmap(QPixmap(":/icons/splash.PNG"));
+    splash->show();
+    Qt::Alignment topRight = Qt::AlignRight | Qt::AlignTop;
+    splash->showMessage(QObject::tr("Loading..."),
+    topRight, Qt::white);
     BUMain w;
     w.show();
     return a.exec();
