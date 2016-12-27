0c241c9e551b3b3edd58d942e10fa6f71c8f5ef6
Jaime Quiroga <gtronick@gmail.com>
2016-12-26 19:47:54 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,14 +4,15 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication a(argc,argv);
     QSplashScreen *splash = new QSplashScreen;
     splash->setPixmap(QPixmap(":/icons/splash.PNG"));
     splash->setWindowFlags(splash->windowFlags() | Qt::WindowStaysOnTopHint);
-    Qt::Alignment topRight = Qt::AlignRight | Qt::AlignTop;
+    Qt::Alignment topRight = Qt::AlignRight | Qt::AlignTop;    
     splash->show();
     a.processEvents();
     splash->showMessage(QObject::tr("Loading..."),topRight, Qt::white);
+    a.processEvents();
     BUMain w;
     w.show();
     splash->finish(&w);
