1ef74e4510e072141340d3a1974cf657061be201
Jaime Quiroga <gtronick@gmail.com>
2017-01-03 10:26:44 -0500

    Changed copy logic

--- worker.cpp
+++ worker.cpp
@@ -26,10 +26,17 @@ void Worker::setFileCounter(int value)
 
 void Worker::worker_slot_setStopFlag(int value)
 {
+    // qDebug() << "worker: setStopFlag SIGNAL received with value: " << value;
     giStopDirCopy = value;
+    if(value == 1){
+        // qDebug() << "worker: Clearing lists";
+        gobDirList->clear();
+        gobFilesList->clear();
+        giTotalFiles = 0;
+    }
 }
 
-void Worker::worker_Slot_copyFile(QString sourceFileOrFolder,QString destinationFolder,int giKeep)
+void Worker::worker_slot_createDirs(QString sourceFileOrFolder,QString destinationFolder,int giKeep)
 {
     if(giKeep == 0){
         if(!copyRecursively(sourceFileOrFolder,destinationFolder)){
@@ -43,9 +50,42 @@ void Worker::worker_Slot_copyFile(QString sourceFileOrFolder,QString destination
 
 void Worker::worker_slot_readyToStartCopy()
 {
-    qDebug() << "worker: readyToStartCopy SIGNAL received";
-    qDebug() << "worker: Emmiting sendDirAndFileList SIGNAL";
+    // qDebug() << "worker: readyToStartCopy SIGNAL received";
+    // qDebug() << "worker: Emmiting sendDirAndFileList SIGNAL";
     emit(worker_signal_sendDirAndFileList(gobDirList,gobFilesList));
+}
+
+void Worker::worker_slot_copyFile(QString srcFilePath, QString tgtFilePath)
+{
+    // qDebug() << "worker: copyFile SIGNAL received with " << srcFilePath << ", " << tgtFilePath;
+    QString fileName;
+
+    fileName = QFileInfo(srcFilePath).baseName()+"."+QFileInfo(srcFilePath).completeSuffix();
+
+    if (QFile::exists(tgtFilePath+"/"+fileName)){
+
+        QFile::remove(tgtFilePath+"/"+fileName);
+    }
+
+    gobFile = new QFile(srcFilePath);
+
+    emit(worker_signal_statusInfo("Copying file #" + QString::number(giTotalFiles + 1) + " : " + fileName));
+
+    QApplication::processEvents();
+    if(giStopDirCopy == 0){
+        if(gobFile->copy(tgtFilePath+"/"+fileName)){
+        QApplication::processEvents();
+            giTotalFiles ++;
+            emit(worker_signal_logInfo(QDateTime::currentDateTime().toString() + " >> File: " + srcFilePath + " << copied to: " + tgtFilePath));
+            emit(worker_Signal_updateProgressBar(giTotalFiles));
+        }
+        else{
+            emit(worker_signal_logInfo(QDateTime::currentDateTime().toString() + " >> ERROR! File: " + fileName + " << has not been copied!"));
+        }
+
+        // qDebug() << "worker: Emmiting SIGNAL copyNextFile";
+        emit(worker_signal_copyNextFile());
+    }
 }
 
 bool Worker::copyRecursively(QString srcFilePath, QString tgtFilePath)
@@ -64,31 +104,11 @@ bool Worker::copyRecursively(QString srcFilePath, QString tgtFilePath)
         }
 
     }else{
-        QString fileName;
-
-        fileName = QFileInfo(srcFilePath).baseName()+"."+QFileInfo(srcFilePath).completeSuffix();
-
-        if (QFile::exists(tgtFilePath+"/"+fileName)){
-
-            QFile::remove(tgtFilePath+"/"+fileName);
-        }
-
-        gobFile = new QFile(srcFilePath);
-
-        emit(worker_signal_statusInfo("Copying file #" + QString::number(giTotalFiles + 1) + " : " + fileName));
 
         QApplication::processEvents();
         if(giStopDirCopy == 0){
             gobFilesList->append(srcFilePath);
             gobDirList->append(tgtFilePath+"/");
-            if(gobFile->copy(tgtFilePath+"/"+fileName)){
-                QApplication::processEvents();
-                giTotalFiles ++;
-                emit(worker_signal_logInfo(QDateTime::currentDateTime().toString() + " >> File: " + srcFilePath + " << copied to: " + tgtFilePath));
-                emit(worker_Signal_updateProgressBar(giTotalFiles));
-            }else{
-                emit(worker_signal_logInfo(QDateTime::currentDateTime().toString() + " >> ERROR! File: " + fileName + " << has not been copied!"));
-            }
         }
     }
 
