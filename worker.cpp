797cb8272bbb0f11d18ff129a34320dae3528c94
Jaime Quiroga <gtronick@gmail.com>
2017-02-02 15:18:54 -0500

    Counting files added and improved.

--- worker.cpp
+++ worker.cpp
@@ -8,12 +8,15 @@
 #include <QDateTime>
 #include <QMessageBox>
 #include <QApplication>
+#include <QDirIterator>
 
 Worker::Worker(QObject *parent) :
     QObject(parent)
 {
     giTotalFiles = 0;
     giStopDirCopy = 0;
+    giFileCounter = 0;
+    giTotalFolders = 0;
     gobDirList = new QStringList;
     gobFilesList = new QStringList;
 
@@ -62,8 +65,12 @@ void Worker::worker_slot_copyFile(QString srcFilePath, QString tgtFilePath)
 {
     // qDebug() << "worker: copyFile SIGNAL received with " << srcFilePath << ", " << tgtFilePath;
     QString fileName;
+    QString extension = "";
+    if(!QFileInfo(srcFilePath).completeSuffix().isEmpty()){
+        extension = "."+QFileInfo(srcFilePath).completeSuffix();
+    }
 
-    fileName = QFileInfo(srcFilePath).baseName()+"."+QFileInfo(srcFilePath).completeSuffix();
+    fileName = QFileInfo(srcFilePath).baseName() + extension;
 
     if (QFile::exists(tgtFilePath+"/"+fileName)){
 
@@ -91,19 +98,54 @@ void Worker::worker_slot_copyFile(QString srcFilePath, QString tgtFilePath)
     }
 }
 
+void Worker::worker_slot_scanFolders(QStringList aobFolderPaths)
+{
+
+    emit(worker_signal_statusInfo("Counting files, please wait..."));
+    giTotalFolders = 0;
+    giFileCounter = 0;
+    int index = 0;
+
+    for(index = 0; index < aobFolderPaths.length(); index++){
+
+        countAllFiles(aobFolderPaths.at(index).trimmed());
+    }
+
+    emit(worker_signal_statusInfo("Ready."));
+    emit(worker_signal_setTotalFilesAndFolders(giFileCounter, giTotalFolders));
+    emit(worker_signal_workerDone());
+}
+
 bool Worker::copyRecursively(QString srcFilePath, QString tgtFilePath)
 {
     QFileInfo source(srcFilePath);
     if(source.isDir()){
         QDir leafDir(srcFilePath);
-        leafDir.setFilter(QDir::AllEntries | QDir::NoDotAndDotDot | QDir::Hidden | QDir::System);
-        if(!leafDir.mkdir(tgtFilePath + QLatin1Char('/') + source.baseName()))
-            return false;
+        leafDir.setFilter(QDir::AllEntries | QDir::NoDotAndDotDot | QDir::Hidden | QDir::System | QDir::NoSymLinks);
+        if(source.baseName().isEmpty()){
+            if(!leafDir.mkdir(tgtFilePath + QLatin1Char('/') + "." + source.completeSuffix())){
+                qDebug() << "Folder " + tgtFilePath + QLatin1Char('/') + "." + source.completeSuffix() + " cannot be copied!";
+                return false;
+            }
 
-        QDir sourceDir(srcFilePath);
-        QStringList fileNames = sourceDir.entryList(QDir::AllEntries | QDir::NoDotAndDotDot | QDir::Hidden | QDir::System);
-        for(int index = 0; index < fileNames.length(); index ++){
-            copyRecursively(sourceDir.absolutePath() + "/" + fileNames.at(index),tgtFilePath + QLatin1Char('/') + source.baseName());
+            QDir sourceDir(srcFilePath);
+            QStringList fileNames = sourceDir.entryList(QDir::AllEntries | QDir::NoDotAndDotDot | QDir::Hidden | QDir::System | QDir::NoSymLinks);
+            for(int index = 0; index < fileNames.length(); index ++){
+                copyRecursively(sourceDir.absolutePath() + "/" + fileNames.at(index),tgtFilePath + QLatin1Char('/') + "." + source.completeSuffix());
+            }
+
+        }else{
+            if(!leafDir.mkdir(tgtFilePath + QLatin1Char('/') + source.baseName())){
+                qDebug() << "Folder " << leafDir << " cannot be copied!";
+                return false;
+            }else{
+
+                QDir sourceDir(srcFilePath);
+                QStringList fileNames = sourceDir.entryList(QDir::AllEntries | QDir::NoDotAndDotDot | QDir::Hidden | QDir::System | QDir::NoSymLinks);
+                for(int index = 0; index < fileNames.length(); index ++){
+                    copyRecursively(sourceDir.absolutePath() + "/" + fileNames.at(index),tgtFilePath + QLatin1Char('/') + source.baseName());
+                }
+            }
         }
 
     }else{
@@ -116,4 +158,25 @@ bool Worker::copyRecursively(QString srcFilePath, QString tgtFilePath)
     }
 
     return true;
+}
+
+void Worker::countAllFiles(QString path)
+{
+
+    QFileInfo source(path);
+    if(source.isDir()){
+        giTotalFolders ++;
+        QDir lobDir(path);
+        lobDir.setFilter(QDir::AllEntries | QDir::NoDotAndDotDot | QDir::Hidden | QDir::System | QDir::NoSymLinks);
+        QStringList files = lobDir.entryList();
+
+        for(int index = 0; index < files.length(); index++){
+            countAllFiles(lobDir.absolutePath() + QLatin1Char('/') + files.at(index));
+        }
+    }else{
+        giFileCounter ++;
+    }
+
+    if(giFileCounter % 10 == 0) emit(worker_signal_setTotalFilesAndFolders(giFileCounter,giTotalFolders));
+
 }
