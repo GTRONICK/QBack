3ba44b64310692808b9ef9d08804467d9471cbbc
Jaime Quiroga <gtronick@gmail.com>
2017-02-02 15:16:58 -0500

    Changed code order.
    Se agrega search/replace dialog, para la sección de las fuentes.

--- bumain.h
+++ bumain.h
@@ -2,13 +2,13 @@
 #define BUMAIN_H
 
 #define TOOLTIP_DURATION 2000
-#define APP_VERSION "110117"
 #include <QMainWindow>
 #include <QThread>
 #include <QFileDialog>
 #include <QStringList>
 #include "worker.h"
 #include "logviewer.h"
+#include "searchdialog.h"
 
 namespace Ui {
 class BUMain;
@@ -32,6 +32,7 @@ signals:
     void main_signal_readyToStartCopy();
     void main_signal_copyFile(QString file,QString path);
     void main_signal_logInfo(QString info);
+    void main_signal_scanFolders(QStringList folderPaths);
 
 private slots:
     void on_backupButton_clicked();
@@ -41,31 +42,30 @@ private slots:
     void on_openTargetButton_clicked();
     void on_fromFilesTextArea_textChanged();
     void on_logViewerButton_clicked();
-
+    void on_actionQuit_triggered();
+    void on_actionAbout_triggered();
+    void on_actionOpen_session_triggered();
+    void on_actionLoad_theme_triggered();
+    void on_actionSave_session_triggered();
+    void on_actionFind_in_sources_triggered();
     void main_slot_showMessage(QString message);
     void main_slot_keepCopying();
     void main_slot_setStatus(QString status);
     void main_slot_receiveDirAndFileList(QStringList *dirs, QStringList *files);
     void main_slot_copyNextFile();
-
-    void on_actionQuit_triggered();
-
-    void on_actionAbout_triggered();
-
-    void on_actionOpen_session_triggered();
-
-    void on_actionLoad_theme_triggered();
-
-    void on_actionSave_session_triggered();
+    void main_slot_resetCursor();
+    void main_slot_setTotalFilesAndFolders(int aiFileCounter, int aiFolderCounter);
+    void main_slot_workerDone();
 
 private:
     void initThreadSetup();
-    int countAllFiles(QString path);
+    unsigned long countAllFiles(QString path);
     void installEventFilters();
     void uninstallEventFilters();
     bool saveSessionToFile(QString filePath);
     void loadSessionFile(QString asFilePath);
     void on_cancelButton_clicked();
+
     Ui::BUMain *ui;
     QThread *thread;
     Worker *worker;
@@ -75,16 +75,16 @@ private:
     LogViewer *gobLogViewer;
     QStringList *targetDirectories;
     QStringList *sourceFiles;
-    int giKeep;
-    int giCurrentPos;
-    int giPreviousPos;
-    int giFileCounter;
-    int giProgress;
-    int giTotalFiles;
-    int giTotalFolders;
-    int validatorFlag;
-    int giCopyFileIndex;
-    bool gbBackcupButtonPressed;
+    SearchDialog *gobSearchDialog;
+
+    int giKeep;                     //Stop copy flag
+    int giCurrentPos;               //Current carret position in the fromFilesTextArea
+    int giFileCounter;              //Total files counter
+    int giProgress;                 //Copy progress counter
+    int giTotalFolders;             //Total folder counter
+    int validatorFlag;              //Backup button disable flag
+    int giCopyFileIndex;            //Index for the current file being copied
+    bool gbBackcupButtonPressed;    //Backup button control flag (true if pressed)
 };
 
 #endif // BUMAIN_H
