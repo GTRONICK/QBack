535ec8143a5e53da609a76d35ff43d787e46c51f
Jaime Quiroga <gtronick@gmail.com>
2017-02-02 15:19:35 -0500

    Added file counting

--- worker.h
+++ worker.h
@@ -20,18 +20,25 @@ signals:
     void worker_signal_statusInfo(QString info);
     void worker_signal_showMessage(QString message);
     void worker_signal_sendDirAndFileList(QStringList *, QStringList *);
+    void worker_signal_setTotalFilesAndFolders(int totalFiles, int totalFolders);
     void worker_signal_copyNextFile();
+    void worker_signal_workerDone();
 
 public slots:
     void worker_slot_setStopFlag(int value);
     void worker_slot_createDirs(QString sourceFileOrFolder, QString destinationFolder, int giKeep);
     void worker_slot_readyToStartCopy();
     void worker_slot_copyFile(QString srcFilePath, QString tgtFilePath);
+    void worker_slot_scanFolders(QStringList aobFolderPaths);
 
 private:
     bool copyRecursively(QString srcFilePath, QString tgtFilePath);
+    void countAllFiles(QString path);
     int giTotalFiles;
     int giStopDirCopy;
+    int giTotalFolders;
+    int giFileCounter;
+
     QFile *gobFile;
     QStringList *gobDirList;
     QStringList *gobFilesList;
