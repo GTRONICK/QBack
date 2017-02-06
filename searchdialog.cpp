afd350ffec187439fd30e242b4d885f7fd9c4286
Jaime Quiroga <gtronick@gmail.com>
2017-02-06 14:54:01 -0500

    Improved search functionality
    Sistema de búsqueda y reemplazo mejorados.

--- searchdialog.cpp
+++ searchdialog.cpp
@@ -8,11 +8,9 @@ SearchDialog::SearchDialog(QWidget *parent) :
     ui(new Ui::SearchDialog)
 {
     ui->setupUi(this);
-}
-
-void SearchDialog::setGlobalText(QPlainTextEdit *textEdit)
-{
-    this->gobTextEdit = textEdit;
+    gbReplaceClicked = false;
+    gbSearchClicked = false;
+    gbReplaceAllClicked = false;
 }
 
 SearchDialog::~SearchDialog()
@@ -22,64 +20,110 @@ SearchDialog::~SearchDialog()
 
 void SearchDialog::on_searchDialog_searchButton_clicked()
 {
-    QList<QTextEdit::ExtraSelection> extraSelections;
-    QTextEdit::ExtraSelection selection;
-    QColor lineColor = QColor(Qt::darkGray).lighter(160);
-    selection.format.setBackground(lineColor);
-    selection.format.setForeground(QColor(0,0,0));
-
-    if(gsFoundText != ui->seachDialog_searchLineEdit->text()){
-        giOcurrencesFound = 0;
-        giLogCursorPos = 0;
-        //qDebug() << "searchDialog: Emmitting resetCursor SIGNAL";
-        emit(search_signal_resetCursor());
-        gsFoundText = ui->seachDialog_searchLineEdit->text();
-        while(gobTextEdit->find(ui->seachDialog_searchLineEdit->text())){
-            giOcurrencesFound ++;
-            selection.cursor = gobTextEdit->textCursor();
-            extraSelections.append(selection);
-        }
-        gobTextEdit->setExtraSelections(extraSelections);
-        //qDebug() << "searchDialog: Emmitting resetCursor SIGNAL";
-        emit(search_signal_resetCursor());
-        if(gobTextEdit->find(ui->seachDialog_searchLineEdit->text())) giLogCursorPos += 1;
-        ui->ocurrencesCounterLabel->setText(QString("%1/%2").arg(giLogCursorPos).arg(giOcurrencesFound));
-    }else{
-        if(!gobTextEdit->find(ui->seachDialog_searchLineEdit->text())){
-            //qDebug() << "searchDialog: Emmitting resetCursor SIGNAL";
-            emit(search_signal_resetCursor());
-            giLogCursorPos = 0;
-            if(gobTextEdit->find(ui->seachDialog_searchLineEdit->text())) giLogCursorPos ++;
-        }else{
-            giLogCursorPos ++;
-        }
-        ui->ocurrencesCounterLabel->setText(QString("%1/%2").arg(giLogCursorPos).arg(giOcurrencesFound));
-    }
+    gbSearchClicked = true;
+    emit(search_signal_getTextEditText());
 }
 
 void SearchDialog::on_searchDialog_replaceButton_clicked()
 {
-    if(!ui->searchDialog_replaceLineEdit->text().isEmpty() && gobTextEdit->textCursor().hasSelection()
-            && gobTextEdit->textCursor().selectedText().toLower() == ui->seachDialog_searchLineEdit->text().toLower()){
-        gobTextEdit->textCursor().insertText(ui->searchDialog_replaceLineEdit->text());
-    }
+    gbReplaceClicked = true;
+    emit(search_signal_getTextEditText());
 }
 
 void SearchDialog::on_searchDialog_replaceAllButton_clicked()
 {
-    if(!ui->searchDialog_replaceLineEdit->text().isEmpty() && gobTextEdit->textCursor().hasSelection()){
-        QList<QTextEdit::ExtraSelection> extraSelections;
-        extraSelections = gobTextEdit->extraSelections();
-        QTextCursor cursor;
+    gbReplaceAllClicked = true;
+    emit(search_signal_getTextEditText());
+}
 
-        for(int i=0; i < extraSelections.length(); i++){
+void SearchDialog::search_slot_setTextEdit(QPlainTextEdit *textEdit)
+{
+    this->gobTextEdit = textEdit;
 
-            cursor = extraSelections.at(i).cursor;
-            cursor.insertText(ui->searchDialog_replaceLineEdit->text());
+    QList<QTextEdit::ExtraSelection> extraSelections;
+    QTextEdit::ExtraSelection selection;
 
+    if(gbReplaceAllClicked){
+        gbReplaceAllClicked = false;
+
+        gobTextEdit->extraSelections().clear();
+        gobTextEdit->textCursor().clearSelection();
+
+        if(!ui->searchDialog_replaceLineEdit->text().isEmpty()){
+
+            emit(search_signal_resetCursor());
+            gsFoundText = ui->seachDialog_searchLineEdit->text();
+            while(gobTextEdit->find(ui->seachDialog_searchLineEdit->text())){
+                selection.cursor = gobTextEdit->textCursor();
+                extraSelections.append(selection);
+            }
+            gobTextEdit->setExtraSelections(extraSelections);
+
+            QTextCursor cursor;
+
+            for(int i=0; i < extraSelections.length(); i++){
+
+                cursor = extraSelections.at(i).cursor;
+                cursor.insertText(ui->searchDialog_replaceLineEdit->text());
+
+            }
         }
 
         gsFoundText = "";
         gobTextEdit->extraSelections().clear();
+        extraSelections.clear();
+
+    }else if(gbSearchClicked){
+        gbSearchClicked = false;
+
+        QColor lineColor = QColor(Qt::darkGray).lighter(160);
+        selection.format.setBackground(lineColor);
+        selection.format.setForeground(QColor(0,0,0));
+        gobTextEdit->extraSelections().clear();
+
+        if(gsFoundText != ui->seachDialog_searchLineEdit->text()){
+            giOcurrencesFound = 0;
+            giLogCursorPos = 0;
+            //qDebug() << "searchDialog: Emmitting resetCursor SIGNAL";
+            emit(search_signal_resetCursor());
+            gsFoundText = ui->seachDialog_searchLineEdit->text();
+            while(gobTextEdit->find(ui->seachDialog_searchLineEdit->text())){
+                giOcurrencesFound ++;
+                selection.cursor = gobTextEdit->textCursor();
+                extraSelections.append(selection);
+            }
+            gobTextEdit->setExtraSelections(extraSelections);
+            //qDebug() << "searchDialog: Emmitting resetCursor SIGNAL";
+            emit(search_signal_resetCursor());
+            if(gobTextEdit->find(ui->seachDialog_searchLineEdit->text())){
+                ui->searchDialog_replaceButton->setEnabled(true);
+                giLogCursorPos += 1;
+            }
+            ui->ocurrencesCounterLabel->setText(QString("%1/%2").arg(giLogCursorPos).arg(giOcurrencesFound));
+        }else{
+            if(!gobTextEdit->find(ui->seachDialog_searchLineEdit->text())){
+                //qDebug() << "searchDialog: Emmitting resetCursor SIGNAL";
+                emit(search_signal_resetCursor());
+                giLogCursorPos = 0;
+                if(gobTextEdit->find(ui->seachDialog_searchLineEdit->text())){
+                    giLogCursorPos ++;
+                }
+            }else{
+                ui->searchDialog_replaceButton->setEnabled(true);
+                giLogCursorPos ++;
+            }
+            ui->ocurrencesCounterLabel->setText(QString("%1/%2").arg(giLogCursorPos).arg(giOcurrencesFound));
+        }
+    }else if(gbReplaceClicked){
+
+        gbReplaceClicked = false;
+
+        if(gsFoundText == ui->seachDialog_searchLineEdit->text()){
+
+            gobTextEdit->textCursor().insertText(ui->searchDialog_replaceLineEdit->text());
+        }
+
+        ui->searchDialog_replaceButton->setEnabled(false);
+
     }
 }
