4c0434c5589d5612b47bc574b89b38763ef22e1c
Jaime Quiroga <gtronick@gmail.com>
2017-05-05 08:24:39 -0500

    Update worker.h
    Se agregan comentarios

--- worker.h
+++ worker.h
@@ -14,6 +14,7 @@ class Worker : public QObject
 public:
     explicit Worker(QObject *parent = 0);
     void setFileCounter(int value);
+    bool createDirectory(QString path);
 
 signals:
     void worker_Signal_updateProgressBar(int value);
@@ -34,25 +35,26 @@ public slots:
     void worker_slot_copyFile(QString srcFilePath, QString tgtFilePath);
     void worker_slot_scanFolders(QString aobFolderPath);
     void worker_slot_scanNextPath();
+    void worker_slot_renameEnable(bool value);
 
 private:
-    bool copyRecursively(QString srcFilePath, QString tgtFilePath);
+    bool copyDirsRecursively(QString srcFilePath, QString tgtFilePath);
     void countAllFiles(QString path);
     void emitCountersSignals();
 
-    int giCurrentFileIndex;        //Current file index for progressbar and logs.
-    int giStopDirCopy;             //Stop copy flag
-    int giFoldersCounter;          //Total number of folders counted
-    int giFileCounter;             //Total number of files counted
-    int giTotalFilesAccumulator;   //Accumulator for total files label
-    qint64 giTotalFilesSize;       //Accumulator for total size label
-    QStringList gobFileList;       //Current dir entryList
-    QDir gobDir;                   //Current dir under scanning
+    int giCurrentFileIndex;         //Current file index for progressbar and logs.
+    int giStopDirCopy;              //Stop copy flag
+    int giFoldersCounter;           //Total number of folders counted
+    int giFileCounter;              //Total number of files counted
+    int giTotalFilesAccumulator;    //Accumulator for total files label
+    int emitFlag;                   //Flag for file counting control
+    bool gbIsRenameEnabled;         //Flag for enable automatic file renaming
+    qint64 giTotalFilesSize;        //Accumulator for total size label
     QDirIterator *gobIterator;      //Global directory iterator for file counting
-    QFile *gobFile;
-    QStringList *gobDirList;
-    QStringList *gobFilesList;
-    int emitFlag;
+    QFile *gobFile;                 //Current file under scanning
+    QStringList *gobDirList;        //Source directories list
+    QStringList *gobFilesList;      //Source files list
+
 };
 
 #endif // WORKER_H
