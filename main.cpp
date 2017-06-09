ecb622535868a488ef9b64cb0e1eacde4be1bd64
Jaime Quiroga <gtronick@gmail.com>
2017-06-08 20:28:58 -0500

    Changed splash image

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc,argv);
     QSplashScreen *splash = new QSplashScreen;
-    splash->setPixmap(QPixmap(":/icons/LOGO.png"));
+    splash->setPixmap(QPixmap(":/icons/QBackLogo.png"));
     splash->setWindowFlags(splash->windowFlags() | Qt::WindowStaysOnTopHint);
     Qt::Alignment topRight = Qt::AlignRight | Qt::AlignTop;    
     splash->show();
